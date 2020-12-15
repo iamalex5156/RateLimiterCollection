8818b091d165bbd06fdf3b443df40d052244a938
Alex <jiangwei_xu@outlook.com>
2020-12-15 09:26:33 +0800

    Impl Token buckets

---
+++ token_bucket_test.go
@@ -0,0 +1,25 @@
+package rlc
+
+import (
+	"testing"
+	"time"
+)
+
+func TestTokenBucket(t *testing.T) {
+	tokenBucket := NewTokenBucket(32, time.Millisecond*100)
+	var quota bool
+	for i := 0; i < 32; i++ {
+		quota = tokenBucket.Ask()
+	}
+	assertEqual(t, quota, true)
+
+	quota = tokenBucket.Ask()
+	assertEqual(t, quota, false)
+
+	time.Sleep(time.Millisecond * 110)
+
+	quota = tokenBucket.Ask()
+	assertEqual(t, quota, true)
+
+	tokenBucket.Take()
+}
